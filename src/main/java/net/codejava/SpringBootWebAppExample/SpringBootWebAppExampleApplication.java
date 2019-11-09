6bc793a4a1aa9fd096922c925e88887b71ebe7e2
sureshtripini <sureshtripini@gmail.com>
2019-11-09 15:22:01 +0000

    checking in spring boot web appliction example code

---
+++ src/main/java/net/codejava/SpringBootWebAppExample/SpringBootWebAppExampleApplication.java
@@ -0,0 +1,12 @@
+package net.codejava.SpringBootWebAppExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebAppExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebAppExampleApplication.class, args);
+	}
+}
