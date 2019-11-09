77acfde8ee1f6a915b4fb474fa04a171ac2ca94d
sureshtripini <sureshtripini@gmail.com>
2019-11-09 15:36:49 +0000

    updated package details

--- src/main/java/com/suresh/SpringBootWebAppExample/SpringBootWebAppExampleApplication.java
+++ src/main/java/com/suresh/SpringBootWebAppExample/SpringBootWebAppExampleApplication.java
@@ -1,4 +1,4 @@
-package net.codejava.SpringBootWebAppExample;
+package com.suresh.SpringBootWebAppExample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
